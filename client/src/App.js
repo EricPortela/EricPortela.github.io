579cc1c3c9a3b98952126f2a410df789fe1d3c48
EricPortela <ericq.portela@gmail.com>
2024-08-20 00:33:30 +0200

    Start of version 2

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import SpeedDial from './components/SpeedDial'
+import Menu from './components/Menu'
+
+function App() {
+  return (
+    <div className="App">
+      <Menu></Menu>
+      <SpeedDial></SpeedDial>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Menu.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Menu.scss'
+
+function Menu() {
+    return ( 
+        <div className='Menu'>
+            <div className='left-div'><p>eric portela | Эрик Портела | 나릭의</p></div>
+            <div className='right-div'>
+                <p>Work</p>
+                <p>Me</p>
+                <p>Other</p>
+            </div>
+        </div>
+     );
+}
+
+export default Menu;

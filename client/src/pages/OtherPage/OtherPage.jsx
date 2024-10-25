92da59e742633aab2c7fcd16354f1536c548d0a1
EricPortela <ericq.portela@gmail.com>
2024-10-25 11:34:35 +0200

    Upated other pages

--- client/src/pages/OtherPage/OtherPage.jsx
+++ client/src/pages/OtherPage/OtherPage.jsx
@@ -1,20 +1,38 @@
 import React, { useState, useRef } from 'react';
 import './OtherPage.scss';
 import Menu from '../../components/general-components/Menu.jsx'
-// import SpeedDial from '../../components/general-components/SpeedDial.jsx'
-import Toggle from '../../components/general-components/Toggle.jsx'
-import HeroCell from '../../components/work-page/hero-cell/HeroSection.jsx';
-import ProjectCells from '../../components/work-page/project-section/ProjectSection.jsx';
-import ExperienceSection from '../../components/work-page/experience-section/ExperienceSection.jsx';
-import animationData from '/Users/ericportela/Documents/GitHub/EricPortelaWebsite/client/src/assets/background-blob.json';
+import { ImageGallery } from "react-image-grid-gallery"; 
+import fondDataMockup from '../../assets/fonddata-mockup.svg'; // Import image directly
 
+// Use require.context to import all images from the gallery folder
+const importAll = (requireContext) => requireContext.keys().map(requireContext);
+const images = importAll(require.context('../../assets/photos', false, /\.(png|jpe?g|svg|HEIC|heic)$/));
 
 function OtherPage() {
 
+  // Map over imported images to create an array of objects for ImageGallery
+  const imagesArray = images.map((src, index) => ({
+    src,
+  }));
+
   return (
     <div className="OtherPage">
-      <Menu></Menu>
-      {/* <SpeedDial></SpeedDial> */}
+      <Menu />
+      <p className='subtitle'>
+        During my spare-time I try to enjoy quality time with friends and family.
+        One of my favorite things to do so, is by travelling and experiencing new cultures and places.
+        Although I'm not by any means measured a professional photographer, I really like documenting and taking photos.
+        <br></br>
+        <br></br>
+        I have had the opportunity to visit quite a few countries, mostly in Europe, Asia and North America.
+        Here are some of my selected photos 😄
+      </p>
+      <ImageGallery
+        imagesInfoArray={imagesArray}
+        columnCount={"auto"}
+        columnWidth={300}
+        gapSize={24}
+      />
     </div>
   );
 }

5cc9732421b7b4cdfe8f2859890a3b587f4584a1
EricPortela <ericq.portela@gmail.com>
2024-10-26 12:30:18 +0200

    Added website links

--- client/src/components/work-page/project-section/project-cell/ProjectCell.jsx
+++ client/src/components/work-page/project-section/project-cell/ProjectCell.jsx
@@ -5,14 +5,30 @@ import {
     spring
   } from "framer-motion";
 
+  function WebsiteButton({ website }) { 
+    if (!website || website.length === 0) { 
+        return null;
+    } else {
+        return (
+            <a className='website-button' target = "_blank" href={website}>
+                <p>Visit website</p>
+            </a>
+        );
+    }
+} 
 
 function ProjectCell({project}) {
+    var projectWebsite = project?.website;
+
     return (
         <div className='ProjectCell'>
             <div className="text-div">
                 <p className='title'>{project?.title}</p>
                 <p className='year'>{project?.startYear} - {project?.endYear}</p>
                 <p className='description'>{project?.description}</p>
+
+                <WebsiteButton website={projectWebsite}/>
+
             </div>
 
             <div className='known-technologies'>
@@ -22,6 +38,7 @@ function ProjectCell({project}) {
             <div className="image-div">
                 <img src={require("../../../../assets/" + project?.imgName)} alt={project?.imgName} />
             </div>
+
         </div>
      );
 }

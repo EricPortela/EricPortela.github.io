6a0acdadea6ff580b2f4a0b964f8d2e13807319c
EricPortela <ericq.portela@gmail.com>
2025-01-27 00:43:37 +0100

    Added a button that can return user to home page in case they break the url

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,14 @@ import {
   useScroll
 } from "framer-motion";
 
-import { Routes, BrowserRouter, Route, HashRouter } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 
-import AboutMe from './pages/AboutMePage/AboutMePage.jsx';
-import Work from './pages/WorkPage/WorkPage.jsx';
-import Other from './pages/OtherPage/OtherPage.jsx';
+import { useLottie } from 'lottie-react';
+import animationData from './assets/loading-page-animation.json';
 import AlertImage from './assets/me-coding-1.jpg';
+import AboutMe from './pages/AboutMePage/AboutMePage.jsx';
+import Other from './pages/OtherPage/OtherPage.jsx';
+import Work from './pages/WorkPage/WorkPage.jsx';
 
 function App() {
 
@@ -20,6 +22,22 @@ function App() {
   };
 
   const { scrollYProgress } = useScroll();
+
+  // const defaultOptions = {
+  //   loop: true,
+  //   autoplay: true,
+  //   animationData: animationData,
+  //   rendererSettings: {
+  //     preserveAspectRatio: "xMidYMid slice"
+  //   }
+  // };
+
+  const options = {
+    animationData: animationData,
+    loop: true
+  };
+
+  const { View } = useLottie(options);
 
   return (
     <div className="App">
@@ -38,8 +56,23 @@ function App() {
           <Route path="/work" element={<Work />} />
           <Route path="/about-me" element={<AboutMe />} />
           <Route path="/other" element={<Other />} />
+
+          <Route
+            path="*"
+            element={
+              <div className="incorrect-page-div">
+                <p className='text-alert'>Oops! You have managed to land on the wrong page.</p>
+                <div className='animationView'>
+                  { View }
+                </div>
+                <Link to="/work" className='back-to-home-button'>
+                  <p>Back to home!</p>
+                </Link>
+              </div>
+            }
+          />
         </Routes>
-      </div>
+      </div>  
     </div>
   );
 }
